356d05e8707a2a3d2e5e0c81198268fce0bea7c9
wkk-knight <2803100746@qq.com>
2020-11-30 16:45:59 +0800

    fix #1

--- MotionLayoutDemo/app/src/main/java/com/wkk/motionlayoutdemo/CodeControlActivity.kt
+++ MotionLayoutDemo/app/src/main/java/com/wkk/motionlayoutdemo/CodeControlActivity.kt
@@ -1,10 +1,13 @@
 package com.wkk.motionlayoutdemo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.constraintlayout.widget.Constraints
+import androidx.core.view.get
 import com.wkk.motionlayoutdemo.databinding.ActivityCodeControlBinding
+import kotlinx.android.synthetic.main.activity_code_control.*
 
 
 /**
@@ -35,8 +38,8 @@ class CodeControlActivity : AppCompatActivity() {
                 //transform 可以改变旋转缩放等属性
                 transform.rotationY = 180f
                 transform.rotationX = 0f
-                transform.scaleY=2f
-                transform.scaleX=2f
+                transform.scaleY = 2f
+                transform.scaleX = 2f
                 propertySet.alpha = 0.1f
             }
 
@@ -53,6 +56,33 @@ class CodeControlActivity : AppCompatActivity() {
                 transform.rotationY = 0f
             }
         }
+
+        /**
+         * fix 动画在执行时，点击动画会重头开始问题
+         * 1.去掉xml中的<OnClick/>
+         * 2.自己编写对应的点击事件控制动画
+         * motionLayout 中有currentState、startState、endState
+         * startState 就是Transition的motion:constraintSetStart设置的id，endState同理
+         * 上述结论 在motionLayout的setTransition(int beginId, int endId)方法中可以看出
+         */
+        binding.root.setOnClickListener {
+//            Log.i(TAG, "currentState: ${binding.motionLayout.currentState}")
+//            Log.i(TAG, "startState: ${binding.motionLayout.startState},R.id.start: ${R.id.start}")
+//            Log.i(TAG, "startState: ${binding.motionLayout.endState},R.id.end: ${R.id.end}")
+
+            //运动中currentState的值是-1 不做处理
+            if (binding.motionLayout.currentState == -1) {
+                return@setOnClickListener
+            }
+
+            if (binding.motionLayout.currentState == binding.motionLayout.startState) {
+                binding.motionLayout.transitionToEnd()
+            } else if (binding.motionLayout.currentState == binding.motionLayout.endState) {
+                binding.motionLayout.transitionToStart()
+            }
+        }
     }
 
 }
+
+private const val TAG = "CodeControlActivity"
