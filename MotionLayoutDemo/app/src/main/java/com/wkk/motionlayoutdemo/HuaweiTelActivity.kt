f250810fd256dce23e4bec80402c321466154b63
wkk-knight <2803100746@qq.com>
2021-05-12 12:55:17 +0800

    :bug: 修复仿华为拨号界面效果bug

--- MotionLayoutDemo/app/src/main/java/com/wkk/motionlayoutdemo/HuaweiTelActivity.kt
+++ MotionLayoutDemo/app/src/main/java/com/wkk/motionlayoutdemo/HuaweiTelActivity.kt
@@ -1,13 +1,14 @@
 package com.wkk.motionlayoutdemo
 
-import android.graphics.drawable.LayerDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.EditText
-import androidx.constraintlayout.motion.widget.MotionLayout
-import androidx.core.content.ContextCompat
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.wkk.motionlayoutdemo.databinding.ActivityHuaweiTelBinding
+import com.wkk.motionlayoutdemo.databinding.ItemTelBinding
+
 /**
  * 仿华为手机EMUI11的拨号界面
  * Created by wkk on 2020/11/9.
@@ -17,5 +18,51 @@ class HuaweiTelActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityHuaweiTelBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        val list = MutableList(50) { "电话号码$it" }
+        binding.recyclerView.adapter = TelAdapter(list)
+
+
+        binding.recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                //RecyclerView 滑动时触发动画，隐藏数字键盘
+                if (newState != RecyclerView.SCROLL_STATE_IDLE && binding.root.currentState == binding.root.startState) {
+                    binding.root.transitionToEnd()
+                }
+            }
+
+        })
+
+        //点击拨号按钮 显示数字键盘
+        binding.imgPhone.setOnClickListener {
+            if (binding.root.currentState == binding.root.endState) {
+                binding.root.transitionToStart()
+            }
+        }
+
+    }
+
+    class TelAdapter(private val tels: List<String>) :
+        RecyclerView.Adapter<TelAdapter.TelViewHolder>() {
+
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = TelViewHolder(parent)
+
+        override fun onBindViewHolder(holder: TelViewHolder, position: Int) {
+            holder.binding.tvTel.text = tels[position]
+        }
+
+        override fun getItemCount() = tels.size
+
+        class TelViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_tel, parent, false)
+        ) {
+            val binding = ItemTelBinding.bind(itemView)
+        }
     }
 }
+
+
+
